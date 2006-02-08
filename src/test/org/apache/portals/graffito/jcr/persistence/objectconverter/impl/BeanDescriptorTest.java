8737c6050ecfbac7902c32a431f596d44f2bc047
Alexandru Popescu <apopescu@apache.org>
2006-02-08 11:13:12 +0000

    bean-descriptor with custom converter + test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/graffito/trunk/jcr/jcr-mapping@375924 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/portals/graffito/jcr/persistence/objectconverter/impl/BeanDescriptorTest.java
+++ src/test/org/apache/portals/graffito/jcr/persistence/objectconverter/impl/BeanDescriptorTest.java
@@ -1,18 +1,49 @@
+/*
+ * Copyright 2000-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.portals.graffito.jcr.persistence.objectconverter.impl;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.apache.portals.graffito.jcr.RepositoryLifecycleTestSetup;
 import org.apache.portals.graffito.jcr.TestBase;
+import org.apache.portals.graffito.jcr.exception.PersistenceException;
+import org.apache.portals.graffito.jcr.mapper.Mapper;
+import org.apache.portals.graffito.jcr.mapper.model.BeanDescriptor;
+import org.apache.portals.graffito.jcr.persistence.objectconverter.BeanConverter;
 import org.apache.portals.graffito.jcr.persistence.objectconverter.ObjectConverter;
 import org.apache.portals.graffito.jcr.testmodel.B;
 import org.apache.portals.graffito.jcr.testmodel.D;
 import org.apache.portals.graffito.jcr.testmodel.DFull;
+import org.apache.portals.graffito.jcr.testmodel.E;
 
 /**
- * This class/interface
+ * ObjectConverter test for bean-descriptor with inner bean inlined and inner bean with
+ * custom converter.
+ * 
+ * @author <a href='mailto:the_mindstorm[at]evolva[dot]ro'>Alexandru Popescu</a>
  */
 public class BeanDescriptorTest extends TestBase {
     private ObjectConverter objectConverter;
@@ -112,7 +143,126 @@ public class BeanDescriptorTest extends TestBase {
         assertNull("b1 was removed", actDFull.getB1());
         assertNull("B.b2 is protected", actDFull.getB2());
 
-        getSession().getItem("/someD").remove();
+    }
+    
+    public void testBeanDescriptorConverter() throws Exception {
+        BeanDescriptor beanDescriptor = this.mapper.getClassDescriptor(E.class).getBeanDescriptor("b1");
+        FakeBeanConverter converter = (FakeBeanConverter) beanDescriptor.getBeanConverter();
+        List messages = new ArrayList();
+        converter.setLog(messages);
+        
+        assertNotNull("E.b1 should be using the FakeBeanConverter", converter);
+        
+        B expB = new B();
+        expB.setB1("b1value");
+        expB.setB2("b2value");
+        E expE = new E();
+        expE.setPath("/someD");
+        expE.setD1("d1value");
+        expE.setB1(expB);
+        
+        this.objectConverter.insert(getSession(), expE);
         getSession().save();
+        
+        // HINT: FakeBeanConverter should set expB
+        converter.setB(expB);
+        E actE = (E) this.objectConverter.getObject(getSession(), E.class, "/someD");
+        
+        
+        assertEquals(expE.getD1(), actE.getD1());
+        assertEquals(expB, actE.getB1());
+        
+        expE.setD1("updatedvalueD1");
+        expB.setB1("updatedvalue1");
+        
+        this.objectConverter.update(getSession(), expE);
+        getSession().save();
+        
+        converter.setB(expB);
+        actE = (E) this.objectConverter.getObject(getSession(), E.class, "/someD");
+        
+        assertEquals(expE.getD1(), actE.getD1());
+        assertEquals(expB, actE.getB1());
+                
+        expE.setB1(null);
+        this.objectConverter.update(getSession(), expE);
+        getSession().save();
+        
+        converter.setB(null);
+        actE = (E) this.objectConverter.getObject(getSession(), E.class, "/someD");
+        
+        assertEquals(expE.getD1(), actE.getD1());
+        assertNull(actE.getB1());
+        
+        // HINT: check messages
+        assertEquals(6, messages.size());
+        assertEquals("insert at path /someD", messages.get(0));
+        assertEquals("get from path /someD", messages.get(1));
+        assertEquals("update at path /someD", messages.get(2));
+        assertEquals("get from path /someD", messages.get(3));
+        assertEquals("remove from path /someD", messages.get(4));
+        assertEquals("get from path /someD", messages.get(5));
+    }
+    
+    public static class FakeBeanConverter implements BeanConverter {
+        private B returnB;
+        private List log;
+        
+        public void setLog(List log) {
+            this.log = log;
+        }
+        
+        public void setB(B b) {
+            this.returnB = b;
+        }
+        
+        /**
+         * @see org.apache.portals.graffito.jcr.persistence.objectconverter.BeanConverter#insert(javax.jcr.Session, javax.jcr.Node, org.apache.portals.graffito.jcr.mapper.Mapper, java.lang.String, java.lang.Object)
+         */
+        public void insert(Session session, Node parentNode, Mapper mapper, String beanName, Object object) throws PersistenceException {
+            try {
+                log.add("insert at path " + parentNode.getPath());
+            }
+            catch(RepositoryException re) {
+                throw new PersistenceException(re);
+            }
+        } 
+
+        /**
+         * @see org.apache.portals.graffito.jcr.persistence.objectconverter.BeanConverter#update(javax.jcr.Session, javax.jcr.Node, org.apache.portals.graffito.jcr.mapper.Mapper, java.lang.String, java.lang.Object)
+         */
+        public void update(Session session, Node parentNode, Mapper mapper, String beanName, Object object) throws PersistenceException {
+            try {
+                log.add("update at path " + parentNode.getPath());
+            }
+            catch(RepositoryException re) {
+                throw new PersistenceException(re);
+            }
+        }
+
+        /**
+         * @see org.apache.portals.graffito.jcr.persistence.objectconverter.BeanConverter#getObject(javax.jcr.Session, javax.jcr.Node, org.apache.portals.graffito.jcr.mapper.Mapper, java.lang.String, java.lang.Class)
+         */
+        public Object getObject(Session session, Node parentNode, Mapper mapper, String beanName, Class beanClass) throws PersistenceException {
+            try {
+                log.add("get from path " + parentNode.getPath());
+            }
+            catch(RepositoryException re) {
+                throw new PersistenceException(re);
+            }
+            return this.returnB;
+        }
+
+        /**
+         * @see org.apache.portals.graffito.jcr.persistence.objectconverter.BeanConverter#remove(javax.jcr.Session, javax.jcr.Node, org.apache.portals.graffito.jcr.mapper.Mapper, java.lang.String)
+         */
+        public void remove(Session session, Node parentNode, Mapper mapper, String beanName) throws PersistenceException {
+            try {
+                log.add("remove from path " + parentNode.getPath());
+            }
+            catch(RepositoryException re) {
+                throw new PersistenceException(re);
+            }
+        }
     }
 }
