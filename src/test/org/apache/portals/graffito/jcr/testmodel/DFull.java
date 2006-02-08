b94ba623858972d87b2eb45637bc02d232d979a6
Alexandru Popescu <apopescu@apache.org>
2006-02-08 10:06:52 +0000

    testmodel for bean-descriptor with inlining
    git-svn-id: https://svn.apache.org/repos/asf/incubator/graffito/trunk/jcr/jcr-mapping@375911 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/portals/graffito/jcr/testmodel/DFull.java
@@ -0,0 +1,68 @@
+package org.apache.portals.graffito.jcr.testmodel;
+
+
+/**
+ * This class/interface
+ */
+public class DFull {
+    private String path;
+    private String d1;
+    private String b1;
+    private String b2;
+
+    /**
+     * @return Returns the b1.
+     */
+    public String getB1() {
+        return b1;
+    }
+
+    /**
+     * @param b1 The b1 to set.
+     */
+    public void setB1(String b1) {
+        this.b1 = b1;
+    }
+
+    /**
+     * @return Returns the b2.
+     */
+    public String getB2() {
+        return b2;
+    }
+
+    /**
+     * @param b2 The b2 to set.
+     */
+    public void setB2(String b2) {
+        this.b2 = b2;
+    }
+
+    /**
+     * @return Returns the dString.
+     */
+    public String getD1() {
+        return this.d1;
+    }
+
+    /**
+     * @param string The dString to set.
+     */
+    public void setD1(String string) {
+        this.d1 = string;
+    }
+
+    /**
+     * @return Returns the path.
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * @param path The path to set.
+     */
+    public void setPath(String path) {
+        this.path= path;
+    }
+}
