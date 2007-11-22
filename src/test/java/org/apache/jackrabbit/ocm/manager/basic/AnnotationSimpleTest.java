fdc4aa9aff6cf17c18004d30c83beaf387658937
Christophe Lombart <clombart@apache.org>
2007-11-22 23:19:45 +0000

    review tearDown method. Call cleanUpRepository
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/sandbox/jackrabbit-jcr-mapping/jcr-mapping@597510 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/jackrabbit/ocm/manager/basic/AnnotationSimpleTest.java
+++ src/test/java/org/apache/jackrabbit/ocm/manager/basic/AnnotationSimpleTest.java
@@ -63,11 +63,7 @@ public class AnnotationSimpleTest extends DigesterTestBase
      */
     public void tearDown() throws Exception
     {
-    	if (getObjectContentManager().objectExists("/test"))
-    	{
-    	   getObjectContentManager().remove("/test");
-    	   getObjectContentManager().save();
-    	}
+    	cleanUpRepisotory();
         super.tearDown();
     }
     
